fffd4cbaf85855d6250f30a72c80ae9ef1a0614b
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2019-11-24 13:18:42 -0500

    add status bar and menu icon for header

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,13 +1,29 @@
 import React from 'react'
-import { Text, View, Button } from 'react-native'
+import { Text, View, Button, StatusBar } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons';
 
 class HomeScreen extends React.Component {
-    static navigationOptions = {
-        title: 'Home',
+    static navigationOptions = ({ navigation }) => {
+        return {
+            title: 'Home',
+            headerLeft: () => (
+                <View style={{ marginLeft: 25 }}>
+                    <Icon
+                        onPress={navigation.openDrawer}
+                        name="ios-menu"
+                        size={35} />
+                </View>
+
+            )
+        }
     };
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <StatusBar
+                    barStyle="dark-content"
+                    backgroundColor="#FFFFFF"
+                />
                 <Text> Home Screen </Text>
                 <Button
                     title="Go to Details"
